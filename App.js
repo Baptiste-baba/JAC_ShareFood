34d917e360d3130450a93d88a18215abb2f59ead
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-03-14 17:37:20 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Pageaccueil from "./Components/Pageaccueil";
+import Search from "./Components/Search";
+import LotsOfStyles from "./Components/Tests";
+import Tests from "./Components/Tests";
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Pageaccueil/>
+    )
+  }
+}
+
