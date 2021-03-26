618e6bf8e2cb455a04a1c05c100e30dbd9e45267
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-03-26 19:03:33 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
 import Page_Main from "./Components/Page_Main";
+import Repas from "./Components/Repas";
 
 
 export default class App extends React.Component {
     render() {
         return (
-            <Navigation/>
+            <Page_Main/>
         )
     }
 }
