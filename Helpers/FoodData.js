07c2d0787c93af4f771d2eca8b5dd18f8aa4e448
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-03-29 00:33:23 +0200

    Add files via upload

--- Helpers/FoodData.js
+++ Helpers/FoodData.js
@@ -1,22 +1,27 @@
 export default data = [
     {
-        id: 100,
+        id: "100",
         food_name: "Salade Composée",
         adress: "36 Avenue de la republique 92320 Chatillon"
     },
     {
-        id: 101,
+        id: "101",
         food_name: "Pizza marguerita",
         adress: "Pizzeria Montparnasse 75010 Paris"
     },
     {
-        id: 102,
+        id: "102",
         food_name: "Sandwich Jambon Fromage",
         adress: "Rue de la lisette 92220 Bagneux"
     },
     {
-        id: 103,
+        id: "103",
         food_name: "Pates fraiches",
         adress: "69 allée des graviés de la Salmouille Gif-Sur-Yvettes"
+    },
+    {
+        id: "104",
+        food_name: "Testfjhfkgjh",
+        adress: "69 allée des graviés de la lskdfjlksfsloirfjiosgsdjgsdlifj"
     },
 ]
