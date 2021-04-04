941aaa958a849de98b1f9df3c08657ab89704c1f
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-04-05 01:05:31 +0200

    Add files via upload

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -3,6 +3,9 @@ import {createAppContainer} from "react-navigation";
 import Login_page from "../Components/Login_page";
 import PageInscription from "../Components/PageInscription";
 import Page_Main from "../Components/Page_Main";
+import Tests from "../Components/Tests";
+import Add_Meal from "../Components/Add_Meal";
+import Cam from "../Components/Cam";
 
 const LoginStackNavigator = createStackNavigator({
     Login_page: {
@@ -19,6 +22,18 @@ const LoginStackNavigator = createStackNavigator({
     },
     Page_Main: {
         screen: Page_Main,
+        navigationOptions: {
+            headerShown: false
+        }
+    },
+    Add_Meal: {
+        screen: Add_Meal,
+        navigationOptions: {
+            headerShown: false
+        }
+    },
+    Cam: {
+        screen: Cam,
         navigationOptions: {
             headerShown: false
         }
