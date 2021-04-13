c188d382375a016847428df9aabdd9e1d069fdb1
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-04-13 02:10:02 +0200

    Add files via upload

--- Helpers/FoodData.js
+++ Helpers/FoodData.js
@@ -2,26 +2,37 @@ export default data = [
     {
         id: "100",
         food_name: "Salade Composée",
-        adress: "36 Avenue de la republique 92320 Chatillon"
+        adress: "36 Avenue de la republique 92320 Chatillon",
+        price: "4"
     },
     {
         id: "101",
         food_name: "Pizza marguerita",
-        adress: "Pizzeria Montparnasse 75010 Paris"
+        adress: "Pizzeria Montparnasse 75010 Paris",
+        price: "5"
     },
     {
         id: "102",
         food_name: "Sandwich Jambon Fromage",
-        adress: "Rue de la lisette 92220 Bagneux"
+        adress: "Rue de la lisette 92220 Bagneux",
+        price: "3,5"
     },
     {
         id: "103",
         food_name: "Pates fraiches",
-        adress: "69 allée des graviés de la Salmouille Gif-Sur-Yvettes"
+        adress: "69 allée des graviés de la Salmouille Gif-Sur-Yvettes",
+        price: "4,3"
     },
     {
         id: "104",
-        food_name: "Testfjhfkgjh",
-        adress: "69 allée des graviés de la lskdfjlksfsloirfjiosgsdjgsdlifj"
+        food_name: "Poulet tandori",
+        adress: "Krakovska 1 11000 Prague Nove Mesto",
+        price: "7"
+    },
+    {
+        id: "105",
+        food_name: "Steak façon boucher",
+        adress: "Avenue des Thernes 75017 Paris",
+        price: "5"
     },
 ]
