f654bbf936c6577668597acc33affa483af77ba3
Baptiste-baba <73906935+Baptiste-baba@users.noreply.github.com>
2021-04-24 15:20:55 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
-import Page_Main from "./Components/Page_Main";
-import Meals from "./Components/Meals";
-import Tests from "./Components/Tests";
-import Add_Meal from "./Components/Add_Meal";
-import Login_page from "./Components/Login_page";
-import PageInscription from "./Components/PageInscription";
-import Cam from "./Components/Cam";
-
 
 export default class App extends React.Component {
     render() {
         return (
-            <Cam/>
+            <Navigation/>
         )
     }
 }
